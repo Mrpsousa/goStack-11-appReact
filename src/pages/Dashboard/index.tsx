9e38c8fcecd8ad0fcd41e6231a84ba9e42428836
Marcos Rogerio <marcos.sousa@lactec.org.br>
2020-11-05 00:35:12 -0300

    feature: conectando à api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import Title from './styles';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoimg from '../../assets/logo.svg';
+import api from '../../services/api';
 
 const Dashboard: React.FC = () => {
-  return <Title> Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoimg} alt="Github Explore" />
+      <Title> Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/574284?s=200&v=4"
+            alt="Roger"
+          />
+          <div>
+            <strong>reposit/qualquer</strong>
+            <p>repositório para teste aleatório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
